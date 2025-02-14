ef39325cb5acf32f592029a763e26ce45ab93667
Jae Young Seo <jaeyoungseo@jys.local>
2025-02-13 22:27:19 -0500

    fixing errors

--- server.js
+++ server.js
@@ -73,6 +73,11 @@ app.post("/api/documents", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
 });
