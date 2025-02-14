0d303ba19562f0f495fff5dcce6cd0b4d4eadbb9
Jae Young Seo <jaeyoungseo@jys.local>
2025-02-14 00:15:14 -0500

    connecting to the right database

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ async function connectToDatabase() {
   try {
     await client.connect();
     console.log("Connected to MongoDB!");
-    return client.db("sampleDatabase").collection("sampleCollection");
+    return client.db("sampleDatabase").collection("testDatabaseWrite");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     throw error;
